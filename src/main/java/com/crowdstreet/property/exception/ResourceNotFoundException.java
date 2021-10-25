d86f767ac7c878ca10a2cd07501801a804e69471
System Administrator <root@Shyamals-MBP.home>
2021-10-25 00:46:45 -0700

    exception

---
+++ src/main/java/com/crowdstreet/property/exception/ResourceNotFoundException.java
@@ -0,0 +1,30 @@
+package com.crowdstreet.property.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionID = 1L;
+
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ResourceNotFoundException(Throwable cause, String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s" , resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+
+}
